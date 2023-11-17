2a480fb2e05786ca03bae2f8f625be2e0490daa7
Oliver Jones <61389067+OliverjonesDev@users.noreply.github.com>
2023-11-17 18:35:39 +0000

    Updated GitIgnore and Large Initial Commit

---
+++ Source/GameJamUE5/GameJamUE5.cpp
@@ -0,0 +1,9 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "GameJamUE5.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, GameJamUE5, "GameJamUE5" );
+
+DEFINE_LOG_CATEGORY(LogGameJamUE5)
+ 
---
+++ Source/GameJamUE5/GameJamUE5GameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "GameJamUE5GameMode.generated.h"
+
+UCLASS(minimalapi)
+class AGameJamUE5GameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	AGameJamUE5GameMode();
+};
+
+
+
