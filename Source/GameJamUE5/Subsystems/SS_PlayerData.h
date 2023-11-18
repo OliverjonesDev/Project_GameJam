41b4ed011a55dc3ab80d5849ccc15898f88d3b41
DevSam2000 <sam.camac@bfb-labs.com>
2023-11-18 06:50:32 +0000

    Set Project to build for mobile (will make it easier to make sure it runs) and added unreal rider for me

---
+++ Source/GameJamUE5/Subsystems/SS_PlayerData.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Subsystems/GameInstanceSubsystem.h"
+#include "SS_PlayerData.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class GAMEJAMUE5_API USS_PlayerData : public UGameInstanceSubsystem
+{
+	GENERATED_BODY()
+	
+};
